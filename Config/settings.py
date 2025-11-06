27db1fc422241f2dbbc86b1a018e0a8916d8cf0a
Xalpha03 <mazxavier3@gmail.com>
2025-11-06 17:39:16 +0000

    fix: add render domain to ALLOWED_HOSTS

--- Config/settings.py
+++ Config/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTSALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'cimgroup.onrender.com,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 
 
